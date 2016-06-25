81a0670ceebdb394fc85d02184fa958d9fa9838a
Jonathan Hall <flimzy@flimzy.com>
2016-06-25 15:04:47 +0200

    Basic client, with basic HTML layout

---
+++ Makefile
@@ -0,0 +1,37 @@
+serve: www
+	go run ./server/*
+
+test: go-test
+
+go-test:
+
+clean:
+	rm -rf www client.js client.js.map bundle.js bundle.js.map pre-bundle.js
+
+node_modules:
+	npm install
+
+bundle.js: client.js
+	browserify --debug . -o pre-bundle.js
+#	cat pre-bundle.js | exorcist bundle.js.map > bundle.js
+	cp pre-bundle.js $@
+
+www/client.js: bundle.js
+	mkdir -p www
+	cp bundle.js $@
+#	uglifyjs bundle.js -c -m -o $@
+# 	uglifyjs bundle.js -c -m -o $@ \
+# 		--source-map @$.map \
+# 		--source-map-root /js \
+# 		--source-map-url /js/client.js.map \
+# 		--in-source-map bundle.js.map
+
+www: www/client.js
+	mkdir -p www
+	cp -a html/* www
+	cp -a vendor/*/* www
+
+client.js: node_modules client/main.go client/main.inc.js
+	gopherjs build client/* -o client.js
+
+all: clean serve
---
+++ client/main.go
@@ -0,0 +1,51 @@
+// +build js
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/gopherjs/jquery"
+	//     "github.com/flimzy/jqeventrouter"
+)
+
+// Some spiffy shortcuts
+var jQuery = jquery.NewJQuery
+var jQMobile *js.Object
+var window *js.Object = js.Global
+var document *js.Object = js.Global.Get("document")
+
+func main() {
+	fmt.Printf("Before ready\n")
+	jQuery(document).Ready(func() {
+		go func() {
+			fmt.Printf("Ready\n")
+			resize()
+			jQuery(window).Resize(resize)
+			jQuery("#wait-message").Hide()
+		}()
+	})
+}
+
+func log(message string) {
+	jQuery("#status").Append(fmt.Sprintf("<p>%s</p>", message))
+}
+
+func resize() {
+	headerHeight := jQuery("#header").Height()
+	bodyHeight := jQuery("body").Height()
+	footerHeight := jQuery("#footer").Height()
+	fmt.Printf("Header height = %d\nBody height = %d\nFooter height = %d\n", headerHeight, bodyHeight, footerHeight)
+	container := jQuery("#container")
+	container.SetHeight(strconv.Itoa(bodyHeight - headerHeight - footerHeight))
+	container.SetCss(map[string]int{"top": headerHeight})
+
+	previewWidth := jQuery("#preview").Width()
+	bodyWidth := jQuery("body").Width()
+	fmt.Printf("Preview width = %d\nBody width = %d\n", previewWidth, bodyWidth)
+	content := jQuery("#content")
+	content.SetWidth(strconv.Itoa(bodyWidth - previewWidth))
+	// 	content.SetCss(map[string]int{"margin-left": previewWidth})
+}

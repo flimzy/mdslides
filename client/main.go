70fdfc5045c4e5e5cfa65a49c330d5b11aa2719f
Jonathan Hall <flimzy@flimzy.com>
2016-06-25 16:35:29 +0200

    Toggle preview menu, add open icon

--- client/main.go
+++ client/main.go
@@ -24,6 +24,10 @@ func main() {
 			fmt.Printf("Ready\n")
 			resize()
 			jQuery(window).Resize(resize)
+			jQuery("#preview-toggle").On("click", previewToggle)
+			jQuery("#open-slideshow").On("click", openSlideshow)
+
+			// Hide the spinner, and show normal content
 			jQuery("#wait-message").Hide()
 			jQuery("#ready-content").Show()
 		}()
@@ -35,18 +39,43 @@ func log(message string) {
 }
 
 func resize() {
-	headerHeight := jQuery("#header").Height()
+	headerHeight := jQuery("#header").OuterHeight()
 	bodyHeight := jQuery("body").Height()
-	footerHeight := jQuery("#footer").Height()
+	footerHeight := jQuery("#footer").OuterHeight()
 	fmt.Printf("Header height = %d\nBody height = %d\nFooter height = %d\n", headerHeight, bodyHeight, footerHeight)
 	container := jQuery("#container")
 	container.SetHeight(strconv.Itoa(bodyHeight - headerHeight - footerHeight))
 	container.SetCss(map[string]int{"top": headerHeight})
 
-	previewWidth := jQuery("#preview").Width()
+	var previewWidth int
+	preview := jQuery("#preview")
+	if preview.Is(":visible") {
+		previewWidth = jQuery("#preview").OuterWidth()
+	}
 	bodyWidth := jQuery("body").Width()
 	fmt.Printf("Preview width = %d\nBody width = %d\n", previewWidth, bodyWidth)
 	content := jQuery("#content")
 	content.SetWidth(strconv.Itoa(bodyWidth - previewWidth))
 	// 	content.SetCss(map[string]int{"margin-left": previewWidth})
+}
+
+func previewToggle(event *js.Object) {
+	toggle := jQuery("#preview-toggle")
+	preview := jQuery("#preview")
+	if preview.Is(":visible") {
+		preview.Hide()
+		toggle.AddClass("fa-angle-down")
+		toggle.RemoveClass("fa-angle-up")
+	} else {
+		preview.Show()
+		toggle.AddClass("fa-angle-up")
+		toggle.RemoveClass("fa-angle-down")
+	}
+	resize()
+	event.Call("preventDefault")
+}
+
+func openSlideshow(event *js.Object) {
+	fmt.Printf("You pressed the open button\n")
+	event.Call("preventDefault")
 }
